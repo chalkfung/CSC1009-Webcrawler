971125bdd1823647968cdbaab8d4943682d71cdd
Chen Yu <scheduledx@gmail.com>
2020-02-07 16:32:04 +0800

    Just added gitignore, not gonna use maven (#2)
    * Just add gitignoren, not marvern yet
    
    * Update vs code lib reference
    
    * Auto format brace placement consistency

--- src/BaseCrawler.java
+++ src/BaseCrawler.java
@@ -6,7 +6,8 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.HashSet;
 
-public class BaseCrawler {
+public class BaseCrawler
+{
     private HashSet<String> links;
 
     public BaseCrawler()
@@ -16,24 +17,25 @@ public class BaseCrawler {
 
     public void getPageLinks(String url)
     {
-        if(!links.contains(url))
+        if (!links.contains(url))
         {
             try
             {
-                if(links.add(url))
+                if (links.add(url))
                     System.out.println(url);
                 Document document = Jsoup.connect(url).get();
                 Elements linksOnPage = document.select("a[href]");
                 for (Element page : linksOnPage)
                     getPageLinks(page.attr("abs:href"));
 
-            }catch (IOException e) {
+            } catch (IOException e)
+            {
                 System.err.println("For '" + url + "': " + e.getMessage());
             }
         }
     }
 
-    public static void main(String [] args)
+    public static void main(String[] args)
     {
         BaseCrawler tmp = new BaseCrawler();
         tmp.getPageLinks("https://sites.google.com/view/loopohkok/home");
