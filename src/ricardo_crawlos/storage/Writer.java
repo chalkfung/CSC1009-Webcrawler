ee3d58d0ad3306e753e838c22a4a53a552bd0797
Chen Yu <scheduledx@gmail.com>
2020-03-09 00:53:38 +0800

    json writer baseline

--- src/ricardo_crawlos/storage/Writer.java
+++ src/ricardo_crawlos/storage/Writer.java
@@ -1,28 +1,11 @@
+package ricardo_crawlos.storage;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Scanner;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import ricardo_crawlos.models.Game;
-
-public class Main
+public class Writer
 {
-    public static void main(String[] args)
-    {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-        Game doto = new Game("doto 3", 1, "volvo", Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3), new Date());
-        Game hl3 = new Game("hl 3", 1, "volvo", Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3), new Date());
-
-        writeAllText("./database/extracted/games/doto.json", gson.toJson(doto));
-        writeAllText("./database/extracted/games/hl3.json", gson.toJson(hl3));
-    }
-
     public static void writeAllText(String path, String text)
     {
         try
