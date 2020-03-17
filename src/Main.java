795e7983c0d94528946a91efc17fd4aaf00b1169
Chen Yu <scheduledx@gmail.com>
2020-03-17 15:59:58 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.xml.stream.FactoryConfigurationError;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.Date;
@@ -7,13 +8,14 @@ import org.jsoup.Jsoup;
 import GUI.MainWindow;
 import ricardo_crawlos.crawlers.GamespotReviewsCrawler;
 import ricardo_crawlos.extractors.GamespotReviewsExtractor;
+import ricardo_crawlos.models.UserReview;
 import ricardo_crawlos.storage.CachedGameSiteCrawler;
 import ricardo_crawlos.storage.JsonSerialiser;
 import ricardo_crawlos.storage.TextWriter;
 
 public class Main
 {
-    public static void main(String[] args) throws IOException, ParseException
+    public static void main(String[] args)
     {
         testExtraction();
 
@@ -28,17 +30,14 @@ public class Main
 
         TextWriter.writeAllText("extractedTest.html", document.html());
 
-        // Elements ratings = document.select("div.media-well--review-user > strong");
         var gamespotDotaReviewsExtractor = new GamespotReviewsExtractor(0);
 
-        var reviewsJson = JsonSerialiser.DefaultInstance().toJson(gamespotDotaReviewsExtractor.extractFrom(document.html()));
+        var extractedReviews = gamespotDotaReviewsExtractor.extractFrom(document.html());
+
+        var reviewsJson = JsonSerialiser.DefaultInstance().toJson(extractedReviews);
 
         TextWriter.writeAllText("database/extracted/reviews/dota-2/gamespot_user-reviews.json", reviewsJson);
-
-        var
     }
-
-
 
     public static void ShowWindow()
     {
