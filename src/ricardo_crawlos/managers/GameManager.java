56951aa2d76a863cae1710a006d619330c88de90
Chen Yu <scheduledx@gmail.com>
2020-03-18 21:08:18 +0800

    Id managers

--- src/ricardo_crawlos/managers/GameManager.java
+++ src/ricardo_crawlos/managers/GameManager.java
@@ -2,21 +2,47 @@ package ricardo_crawlos.managers;
 
 import ricardo_crawlos.core.IManager;
 import ricardo_crawlos.models.Game;
-import java.util.HashMap;
+import ricardo_crawlos.storage.JsonSerialiser;
+import ricardo_crawlos.storage.TextWriter;
 
-public class GameManager implements IManager<Game,String>{
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import com.google.gson.reflect.TypeToken;
+
+public class GameManager implements IManager<Game, String>
+{
 
     private static GameManager gameManagerInstance = null;
-    private HashMap<String, Integer> gameMap;
+    private Map<String, Integer> gameMap;
+
+    private String savePath = "database/managers/game_manager.json";
 
     private GameManager()
     {
-        this.gameMap = new HashMap<String, Integer>();
+        gameMap = new HashMap<>();
+
+        var path = Path.of(savePath);
+        if (Files.exists(path))
+        {
+            try
+            {
+                Type type = new TypeToken<Map<String, Integer>>(){}.getType();
+                gameMap = JsonSerialiser.DefaultInstance().fromJson(Files.readString(path), type);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static GameManager getInstance()
     {
-        if(gameManagerInstance == null)
+        if (gameManagerInstance == null)
             gameManagerInstance = new GameManager();
         return gameManagerInstance;
     }
@@ -33,6 +59,11 @@ public class GameManager implements IManager<Game,String>{
 
     public void append(String name)
     {
-        this.gameMap.putIfAbsent(name, this.gameMap.size());
+        if (!isExist(name))
+        {
+            this.gameMap.put(name, this.gameMap.size());
+            var serialised = JsonSerialiser.DefaultInstance().toJson(this.gameMap);
+            TextWriter.writeAllText(savePath, serialised);
+        }
     }
 }
--- src/ricardo_crawlos/managers/GenreManager.java
+++ src/ricardo_crawlos/managers/GenreManager.java
@@ -2,17 +2,42 @@ package ricardo_crawlos.managers;
 
 import ricardo_crawlos.core.IManager;
 import ricardo_crawlos.models.Genre;
+import ricardo_crawlos.storage.JsonSerialiser;
+import ricardo_crawlos.storage.TextWriter;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.Map;
+import com.google.gson.reflect.TypeToken;
 
 public class GenreManager implements IManager<Genre, String>
 {
 
     private static GenreManager gmInstance = null;
-    private HashMap<String, Integer> genreMap;
+    private Map<String, Integer> genreMap;
+
+    private String savePath = "database/managers/genre_manager.json";
 
     private GenreManager()
     {
-        this.genreMap = new HashMap<String, Integer>();
+        this.genreMap = new HashMap<>();
+
+        var path = Path.of(savePath);
+        if (Files.exists(path))
+        {
+            try
+            {
+                Type type = new TypeToken<Map<String, Integer>>(){}.getType();
+                genreMap = JsonSerialiser.DefaultInstance().fromJson(Files.readString(path), type);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static GenreManager getInstance()
@@ -34,6 +59,11 @@ public class GenreManager implements IManager<Genre, String>
 
     public void append(String name)
     {
-        this.genreMap.putIfAbsent(name, this.genreMap.size());
+        if (!isExist(name))
+        {
+            this.genreMap.put(name, this.genreMap.size());
+            var serialised = JsonSerialiser.DefaultInstance().toJson(this.genreMap);
+            TextWriter.writeAllText(savePath, serialised);
+        }
     }
 }
