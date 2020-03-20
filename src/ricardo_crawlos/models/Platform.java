a3808b6f9a59648ebf128d68ec71db7cd1fa43cc
chalkfung <chalkfung@gmail.com>
2020-03-20 21:31:45 +0800

    Comments again

--- src/ricardo_crawlos/models/Platform.java
+++ src/ricardo_crawlos/models/Platform.java
@@ -1,19 +1,37 @@
 package ricardo_crawlos.models;
 
+/**
+ * Platform class
+ * To hold the game platforms and their given ID
+ */
 public class Platform {
     private String platformName;
     private int platformId;
+
+    /**
+     * Constructor for Platform class
+     * @param platformName_ Platform name
+     * @param platformId_ Platform ID after mapping
+     */
     public Platform(String platformName_, int platformId_ )
     {
         this.platformName = platformName_;
         this.platformId = platformId_;
     }
 
+    /**
+     * Getter of the platform name
+     * @return Platform name in String
+     */
     public String getPlatformName()
     {
         return this.platformName;
     }
 
+    /**
+     * Getter of the platform ID after mapping
+     * @return Platform ID after mapping
+     */
     public int getPlatformId()
     {
         return this.platformId;
